2f729e5eac2ee03f38a84321fe491ce02cd7cfb0
Yunda03 <a01034353045@gmail.com>
2024-08-13 21:00:48 +0900

    Jinpoy 집에서 커밋

--- src/reservation/pages/ReservationMain.js
+++ src/reservation/pages/ReservationMain.js
@@ -8,17 +8,65 @@ const OuterBox = styled.div`
   margin-bottom: 150px;
 `;
 
+const PageNavWrap = styled.div`
+  box-sizing: border-box;
+  padding: 0 20px 20px 0;
+`;
+
+const PageNavSubWrap = styled.div`
+  border-bottom: solid 1.5px rgb(221, 221, 221);
+  max-width: 1200px;
+  padding: 48px 0;
+  position: relative;
+  margin: 0 auto;
+`;
+
+const PageNav = styled.div`
+  line-height: normal;
+`;
+
+const PageTitle = styled.div`
+  margin-top: 20px;
+`;
+
+const ContentBox = styled.div`
+  box-sizing: border-box;
+  width: 1200px;
+  min-height: 700px;
+  padding: 0 20px 50px 20px;
+  margin: 0 auto;
+  border: solid 1.5px rgb(221, 221, 221);
+  text-align: center;
+  align-item: center;
+  img {
+    width: 50%;
+  }
+`;
+
 const ReservationMain = () => {
   const { t } = useTranslation();
   return (
-    <MemberOnlyContainer>
+    <>
       <Helmet>
-        <title>{t('농촌체험 예약')}</title>
+      <title>{t('농촌체험 예약')}</title>
       </Helmet>
-      <OuterBox className="layout-width">
-        <h1>농촌체험 예약</h1>
+      <OuterBox>
+        <PageNavWrap>
+          <PageNavSubWrap>
+            <PageNav>
+              <h3>이동 / 이동 / 이동</h3>
+            </PageNav>
+            <PageTitle>
+            <h1>농촌체험 예약</h1>
+            </PageTitle>
+          </PageNavSubWrap>
+        </PageNavWrap>
+        <ContentBox>
+          <h2>지도 클릭하면 지역별 축제를 검색합니다.</h2>
+          <img src={process.env.PUBLIC_URL + '/무색.png'}/>
+        </ContentBox>
       </OuterBox>
-    </MemberOnlyContainer>
+    </>
   );
 };
 
