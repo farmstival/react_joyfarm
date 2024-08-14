17d3f9fca0a371e42f13b4eac665c7a45717ca6c
soeunl <soeun318@gmail.com>
2024-08-14 16:22:19 +0900

    추천 여행지 리스트 출력, 간단 디자인, 상세페이지 연결

--- src/recommend/tour/containers/ListContainer.js
+++ src/recommend/tour/containers/ListContainer.js
@@ -1,11 +1,25 @@
 import React, { useEffect, useState, useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { apiList } from '../apis/apiInfo';
 import ItemsBox from '../components/ItemsBox';
 import SearchBox from '../components/SearchBox';
 import Pagination from '../../../commons/components/Pagination';
 
+function getQueryString(searchParams) {
+  const qs = {};
+  if (searchParams.size > 0) {
+    for (const [k, v] of searchParams) {
+      qs[k] = v;
+    }
+  }
+  return qs;
+}
+
 const ListContainer = () => {
-  const [search, setSearch] = useState({});
+  const [searchParams] = useSearchParams();
+
+  const [form, setForm] = useState(() => getQueryString(searchParams));
+  const [search, setSearch] = useState(() => getQueryString(searchParams));
   const [items, setItems] = useState([]);
   const [pagination, setPagination] = useState({});
 
@@ -16,11 +30,34 @@ const ListContainer = () => {
     });
   }, [search]);
 
+  /* 검색 관련 함수 */
+  const onChangeSearch = useCallback((e) => {
+    setForm((form) => ({ ...form, [e.target.name]: [e.target.value] }));
+  }, []);
+
+  const onSubmitSearch = useCallback(
+    (e) => {
+      e.preventDefault();
+      setSearch({ ...form, page: 1 });
+    },
+    [form],
+  );
+
+  /* 페이지 변경 함수 */
+  const onChangePage = useCallback((p) => {
+    setSearch((search) => ({ ...search, page: p }));
+  }, []);
   return (
     <>
-      <SearchBox search={search} />
+      <SearchBox
+        form={form}
+        onChange={onChangeSearch}
+        onSubmit={onSubmitSearch}
+      />
       <ItemsBox items={items} />
-      <Pagination />
+      {items.length > 0 && (
+        <Pagination onClick={onChangePage} pagination={pagination} />
+      )}
     </>
   );
 };
