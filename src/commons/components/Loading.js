55941f14f8f23e07b0ecdba7261bcfb08e7ffa6d
beom33 <byby5546@naver.com>
2024-08-17 14:38:55 +0900

    ..

--- src/commons/components/Loading.js
+++ src/commons/components/Loading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../../images/Loading.webp';
+import image from '../../images/loading.webp';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
