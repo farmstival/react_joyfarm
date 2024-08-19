b3000d6431c33dcd7831d872a1b63777a61c0920
kth232 <kth7537@naver.com>
2024-08-19 15:09:40 +0900

    이메일 인증 추가 중

--- src/member/apis/apiJoin.js
+++ src/member/apis/apiJoin.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import apiRequest from '../../commons/libs/apiRequest';
 import cookies from 'react-cookies';
 
@@ -17,4 +18,29 @@ export const apiJoin = (form) =>
       .catch((err) => {
         reject(err);
       });
+  });
+
+  // 이메일 인증 메일 보내기
+export const apiEmailAuth = (email, uid) =>
+  requestData(`/email/verify?email=${email}&uid=${uid}`);
+
+// 인증 메일 코드 검증 처리
+export const apiEmailAuthCheck = (authNum, uid) =>
+  new Promise((resolve, reject) => {
+    (async () => {
+      try {
+        const res = await apiRequest(
+          `/email/auth_check?authNum=${authNum}&uid=${uid}`,
+        );
+
+        if (res.status === 200 && res.data.success) {
+          reject(res.data);
+          return;
+        }
+
+        resolve(res.data.data);
+      } catch (err) {
+        reject(err);
+      }
+    })();
   });
