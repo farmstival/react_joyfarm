6858bc87d510dab84165d605c228d098a806b71e
hyeon12 <seojh912_@naver.com>
2024-08-20 12:34:01 +0900

    - 축제 검색바 css 추가

--- src/recommend/Festival/components/SearchBox.js
+++ src/recommend/Festival/components/SearchBox.js
@@ -2,9 +2,46 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { MidButton } from '../../../commons/components/Buttons';
+import { color } from '../../../styles/color';
+const {white, midGreen} = color;
 
 const FormBox = styled.form`
   width: 1300px;
+
+  .tour-search {
+    display: flex;
+    gap: 5px;
+    justify-content: flex-end;
+    align-items: center;
+    height: 80px;
+    border: 1px solid #ccc;
+    border-top: 3px solid #444;
+    padding: 5px 20px;
+
+    select {
+    width: 120px;
+    height: 45px;
+    padding: 0 10px;
+    border: 1px solid #ccc;
+    }
+
+    input {
+    width: 250px;
+    height: 45px;
+    border: 1px solid #ccc;
+    padding: 0 10px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  font-size: 1.3em;
+  color: ${white};
+  width: 80px;
+  height: 45px;
+  background: ${midGreen};
+  border: none;
+  cursor: pointer;
 `;
 
 const SearchBox = ({ form, onChange, onSubmit }) => {
@@ -12,7 +49,7 @@ const SearchBox = ({ form, onChange, onSubmit }) => {
 
   return (
     <FormBox onSubmit={onSubmit} autoComplete="off">
-      <div className="selectBox">
+      <div className="tour-search">
         <select name="sopt" onChange={onChange} defaultValue={form.sopt}>
           <option value="ALL">{t('통합검색')}</option>
           <option value="TITLE">{t('축제명')}</option>
@@ -25,9 +62,10 @@ const SearchBox = ({ form, onChange, onSubmit }) => {
           name="skey"
           value={form.skey}
           onChange={onChange}
+          placeholder={t('검색어를_입력하세요')}
         />
+      <Button>검색하기</Button>
       </div>
-      <MidButton color="midGreen">검색하기</MidButton>
     </FormBox>
   );
 };
