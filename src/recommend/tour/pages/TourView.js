7aa6daae66d50fa5e1220d2d89ed3c72e01f8d57
Yunda0204 <a01034353045@gmail.com>
2024-08-20 09:26:38 +0900

    no message

--- src/recommend/tour/pages/TourView.js
+++ src/recommend/tour/pages/TourView.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-<<<<<<< HEAD
-import { OuterBox } from '../../../commons/components/LayoutBox';
-=======
-import { PageNavWrap, ContentBox, OuterBox } from '../../../commons/components/LayoutBox';
->>>>>>> ae8cc0939bdab9bb11fb2850d4f4c59698611293
+import {
+  PageNavWrap,
+  ContentBox,
+  OuterBox,
+} from '../../../commons/components/LayoutBox';
 import { MainTitle } from '../../../commons/components/TitleBox';
 import ViewContainer from '../containers/ViewContainer';
 
@@ -17,19 +17,12 @@ const TourView = () => {
         <title>{pageTitle}</title>
       </Helmet>
       <OuterBox>
-<<<<<<< HEAD
-        {' '}
-        // css 스타일 에밥마
-        <MainTitle>{pageTitle}</MainTitle>
-        <ViewContainer setPageTitle={setPageTitle} />
-=======
-      <PageNavWrap>
-        <MainTitle>{pageTitle}</MainTitle>
+        <PageNavWrap>
+          <MainTitle>{pageTitle}</MainTitle>
         </PageNavWrap>
         <ContentBox>
-        <ViewContainer setPageTitle={setPageTitle} />
+          <ViewContainer setPageTitle={setPageTitle} />
         </ContentBox>
->>>>>>> ae8cc0939bdab9bb11fb2850d4f4c59698611293
       </OuterBox>
     </>
   );
