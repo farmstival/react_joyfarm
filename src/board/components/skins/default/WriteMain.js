41dee66f31f71b66028bdee8fb3dda5872fe23da
soeunl <154653812+soeunl@users.noreply.github.com>
2024-08-23 20:43:20 +0900

    Update WriteMain.js

--- src/board/components/skins/default/WriteMain.js
+++ src/board/components/skins/default/WriteMain.js
@@ -1,28 +1,8 @@
 import React from 'react';
 import Form from './Form';
 
-const WriteMain = ({
-  board,
-  form,
-  onSubmit,
-  onToggleNotice,
-  errors,
-  fileUploadCallback,
-  fileDeleteCallback,
-  onChange,
-}) => {
-  return (
-    <Form
-      board={board}
-      form={form}
-      onSubmit={onSubmit}
-      onToggleNotice={onToggleNotice}
-      errors={errors}
-      fileUploadCallback={fileUploadCallback}
-      fileDeleteCallback={fileDeleteCallback}
-      onChange={onChange}
-    />
-  );
+const WriteMain = (props) => {
+    return <Form {...props} />;
 };
 
 export default React.memo(WriteMain);
