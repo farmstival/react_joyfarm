b25558a9050e93c18f9fc3abb938e26f448466de
soeunl <soeun318@gmail.com>
2024-08-23 16:56:13 +0900

    찜하기 기능 구현 완료

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import ErrorPage from './commons/pages/Error';
 import { UserInfoProvider } from './member/modules/UserInfoContext';
 import { RecommendProvider } from './commons/modules/RecommendContext';
+import { WishListProvider } from './commons/contexts/WishListContext';
 
 import 'react-calendar/dist/Calendar.css'; //calendar 기본 스타일 시트 전역 적용
 
@@ -17,6 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserInfoProvider>
+      <WishListProvider>
       <RecommendProvider>
         <ErrorPage>
           <HelmetProvider>
@@ -26,6 +28,7 @@ root.render(
           </HelmetProvider>
         </ErrorPage>
       </RecommendProvider>
+      </WishListProvider>
     </UserInfoProvider>
   </React.StrictMode>,
 );
