13596a7a165009def08dbb4a78f031853dc93a63
heyejiyang <xoxo99312@naver.com>
2024-08-26 21:08:25 +0900

    글 수정, 목록이동, 조회

---
+++ src/board/components/skins/default/CommentForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommentForm = () => {
+  return <>작성양식</>;
+};
+
+export default React.memo(CommentForm);
---
+++ src/board/containers/ViewContainer.js
@@ -0,0 +1,72 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import ViewContent from '../components/skins/default/ViewContent';
+import CommentForm from '../components/skins/default/CommentForm';
+import CommentItems from '../components/skins/default/CommentItems';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { getInfo } from '../apis/apiBoard';
+import Loading from '../../commons/components/Loading';
+import MessageBox from '../../commons/components/MessageBox';
+import { info } from 'sass';
+
+const ViewContainer = ({ setPageTitle }) => {
+  const { seq } = useParams();
+  const [board, setBoard] = useState(null);
+  const [data, setData] = useState(null);
+  const [message, setMessage] = useState('');
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getInfo(seq);
+        setData(res);
+        setBoard(res.board);
+        setPageTitle(res.subject);
+      } catch (err) {
+        console.error(err);
+        setMessage(err.message);
+        setTimeout(function () {
+          setMessage('');
+          navigate(-1);
+        }, 3000);
+      }
+    })();
+  }, [seq, setPageTitle, navigate, setMessage, message]);
+
+  const onDelete = useCallback(
+    (seq) => {
+      if (!window.confirm(t('정말_삭제_하겠습니까?'))) {
+        return;
+      }
+      console.log(seq);
+    },
+    [t],
+  );
+
+  if (!data) {
+    return (
+      <>
+        {message && <MessageBox color="info">{message}</MessageBox>}
+        <Loading />;
+      </>
+    );
+  }
+
+  const { useComment } = board;
+
+  return (
+    <>
+      <ViewContent data={data} onDelete={onDelete} />
+      {useComment && (
+        <>
+          {data.isCommentable && <CommentForm />}
+          {data?.comments?.length > 0 && <CommentItems items={data.comments} />}
+        </>
+      )}
+    </>
+  );
+};
+
+export default React.memo(ViewContainer);
