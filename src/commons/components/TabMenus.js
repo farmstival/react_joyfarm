39965b626f8964fa5a3d39078749b0047aeb1a9a
soeunl <soeun318@gmail.com>
2024-08-26 15:54:22 +0900

    찜하기 리스트 탭메뉴 디자인

--- src/commons/components/TabMenus.js
+++ src/commons/components/TabMenus.js
@@ -2,36 +2,44 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 import styled from 'styled-components';
-import { buttonColor } from '../../styles/color';
+import { color } from '../../styles/color';
 import fontSize from '../../styles/fontSize';
-const { midGreen, darkGreen } = buttonColor;
+
+const { midGreen, line_gray, dark, white } = color;
 const { big } = fontSize;
 
 const Wrapper = styled.nav`
   padding: 10px 0;
   display: flex;
+  align-items: center;
+  justify-content: center;
   height: 55px;
   margin-bottom: 20px;
+
   a {
-    background: ${midGreen[0]};
+    background: ${white};
     width: 150px;
     height: 50px;
-    color: ${darkGreen[1]};
+    color: ${dark};
     padding: 0 25px;
     border-radius: 5px;
     font-size: ${big};
     display: flex;
     align-items: center;
     justify-content: center;
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+    border: 1px solid ${line_gray};
   }
   a:hover,
   a.on {
-    background: ${darkGreen[2]};
+    background: ${midGreen};
+    color: ${white};
+    border: none;
     transform: scale(1.05);
     box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
   }
   a + a {
-    margin-left: 10px;
+    margin-left: 20px;
   }
 `;
 
