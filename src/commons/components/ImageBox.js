e383262159c952a7baee487c5fffd683702e2d56
soeunl <154653812+soeunl@users.noreply.github.com>
2024-08-28 20:58:27 +0900

    Update ImageBox.js

--- src/commons/components/ImageBox.js
+++ src/commons/components/ImageBox.js
@@ -2,11 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const ImageBgBox = styled.div`
-
-  background:url('${({url}) => url}') no-repeat center center;
-  background-size: cover;
-  width: ${({width}) => width ?? '200px'};
-  height: ${({height}) => height ?? '200px'};
   background: url('${({ url }) => url}') no-repeat center center;
   background-size: 100% 100%;
   width: ${({ width }) => width ?? '200px'};
