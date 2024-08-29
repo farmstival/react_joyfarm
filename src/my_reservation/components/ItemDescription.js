7660fc544ac62732f9bf85a1bbc856d0a4432019
beom33 <byby5546@naver.com>
2024-08-29 18:35:15 +0900

    최신사항 반영

--- src/my_reservation/components/ItemDescription.js
+++ src/my_reservation/components/ItemDescription.js
@@ -79,10 +79,6 @@ const ItemDescription = ({ item, onClick }) => {
   const { t } = useTranslation();
   const { name, email, mobile, rdate, ampm, townName, persons, status } = item;
   const formatDate = format(Date(rdate), 'yyyy-MM-dd');
-
-  const formatDate = format(
-    Date(rdate), 'yyyy-MM-dd');
-
   return (
     <Wrapper>
       <div className="dscp_box">
