542e43248c028916d3aaa588eab56717f76a6666
Yunda0204 <a01034353045@gmail.com>
2024-08-30 15:19:05 +0900

    .

--- src/my_reservation/pages/MyReserveView.js
+++ src/my_reservation/pages/MyReserveView.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useTranslation } from 'react-i18next';
 import {
   OuterBox,
-  PageNav,
-  PageNavWrap,
   PageTitle,
   ContentBox,
 } from '../../commons/components/LayoutBox';
-import { MainTitle } from '../../commons/components/TitleBox';
 import MyReserveViewContainer from '../containers/MyReserveViewContainer';
-import { Link, NavLink } from 'react-router-dom';
-import { SubTitle } from '../../commons/components/TitleBox';
 import MemberOnlyContainer from '../../member/containers/MemberOnlyContainer';
 import Header from '../../layouts/Header';
 import SubTitleLink from '../../commons/SubTitleLink';
