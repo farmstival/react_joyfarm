295264fdca9a05f9c8780c8a62fe2c7f53d18f90
beom33 <byby5546@naver.com>
2024-08-30 18:51:30 +0900

    최신사항 반영

--- src/mypage/apis/apiMyPage.js
+++ src/mypage/apis/apiMyPage.js
@@ -1,6 +1,43 @@
-import requestData from '../../commons/libs/requestData';
-import saveProcess from '../../commons/libs/saveProcess';
+import apiRequest from '../../commons/libs/apiRequest';
 
-// 회원정보 수정
-export const updateProfile = (form) =>
-  saveProcess('/mypage/profile', 'PATCH', form);
+export const apiUpdate = (form) =>
+  new Promise((resolve, reject) => {
+    console.log(form);
+
+    apiRequest('/account/update', 'PUT', form)
+      .then((res) => {
+        if (![201, 204, 200].includes(res.status)) {
+          // 검증 실패
+
+          reject(res.data);
+          return;
+        }
+
+        resolve(res.data); // 성공
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
+
+export const apiPatch = (form) =>
+  new Promise((resolve, reject) => {
+    console.log(form);
+
+    apiRequest('/account/withdraw', 'PATCH')
+      .then((res) => {
+        if (![201, 204, 200].includes(res.status)) {
+          // 검증 실패
+
+          reject(res.data);
+          return;
+        }
+
+        resolve(res.data); // 성공
+      })
+      .catch((err) => {
+        console.error(err);
+        reject(err);
+      });
+  });
