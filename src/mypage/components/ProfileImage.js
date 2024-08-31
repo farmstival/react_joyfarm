54fc4615174e6deb7b873cbf5d2563854a5f73cd
beom33 <byby5546@naver.com>
2024-08-31 12:55:34 +0900

    ..

--- src/mypage/components/ProfileImage.js
+++ src/mypage/components/ProfileImage.js
@@ -8,17 +8,14 @@ const Wrapper = styled.div`
   height: 200px;
   margin: auto;
 
-
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: contain; // cover: 이미지를 컴포넌트에 맞게 잘라냄
-                     // contain: 이미지를 컴포넌트 크기에 맞춰 축소
-  border-radius: 20%; // 동그란 프사를 원하면 추가
-}
-
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain; // cover: 이미지를 컴포넌트에 맞게 잘라냄
+    // contain: 이미지를 컴포넌트 크기에 맞춰 축소
+    border-radius: 20%; // 동그란 프사를 원하면 추가
+  }
 `;
-
 
 const ProfileImage = ({ gid, profileImage, fileUploadCallback, className }) => {
   return (
@@ -32,7 +29,7 @@ const ProfileImage = ({ gid, profileImage, fileUploadCallback, className }) => {
         done={true} // false 로 바꾸면 수정하기 눌러야 프로필이미지 수정됨!
         callback={fileUploadCallback}
       />
-       <p style={{ marginTop: '14px', fontSize: '12px', color: '#428C4C' }}>
+      <p style={{ marginTop: '14px', fontSize: '12px', color: '#428C4C' }}>
         🍃프로필 변경시 사진을 클릭해주세요
       </p>
     </Wrapper>
